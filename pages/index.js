52ad90e9937b074842354a9b6d7eade72487538b
nisharrk <nishantsharma2633@gmail.com>
2023-02-16 23:11:52 -0500

    working final

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ * WEB422 – Assignment 3
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+ * No part of this assignment has been copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Name: Nishant Sharma   Student ID: 150619203   Date: 02/16/2023
+ * Github Link:
+ *
+ ********************************************************************************/
+
 import React from "react";
 import useSWR from "swr";
 import { useState, useEffect } from "react";
