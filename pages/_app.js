a760886191607f7226b5f0f533176f5a0a490468
nisharrk <nishantsharma2633@gmail.com>
2023-02-16 23:06:46 -0500

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { SWRConfig } from "swr";
+import Layout from "../src/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <SWRConfig
+        value={{
+          fetcher: (...args) => fetch(...args).then((res) => res.json()),
+        }}
+      >
+        <Component {...pageProps} />
+      </SWRConfig>
+    </Layout>
+  );
 }
